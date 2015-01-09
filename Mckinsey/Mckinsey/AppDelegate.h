f15608c12c3ac873437efea87620e87775f7e540
Rakesh Mahato <rakeshmahato@Rakeshs-MacBook-Pro.local>
2015-01-09 11:54:05 +0530

    Added all files to repo

---
+++ Mckinsey/Mckinsey/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Mckinsey
+//
+//  Created by Rakesh Mahato on 09/01/15.
+//  Copyright (c) 2015 Rakesh Mahato. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Mckinsey/Mckinsey/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Mckinsey
+//
+//  Created by Rakesh Mahato on 09/01/15.
+//  Copyright (c) 2015 Rakesh Mahato. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
